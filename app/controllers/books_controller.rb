138ed5b1c6a1e7b6858be7773f951b0de0ac630d
yoshihiro201104 <187289075+yoshihiro201104@users.noreply.github.com>
2024-12-04 07:41:43 +0000

    [Add] 9-bookers

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -24,6 +24,19 @@ class BooksController < ApplicationController
   def edit
     @book = Book.find(params[:id])
   end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)  
+  end
+
+  def destroy
+    book = Book.find(params[:id])  # データ（レコード）を1件取得
+    book.destroy  # データ（レコード）を削除
+    redirect_to '/books'  # 投稿一覧画面へリダイレクト 
+  end
+
   private
   # ストロングパラメータ
   def book_params
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,9 @@ Rails.application.routes.draw do
   get 'books/edit'
   resources :books
   get 'top' => 'homes#top'
+  patch 'books/:id' => 'books#update', as: 'update_book'
+  delete 'book/:id' => 'books#destroy', as: 'destroy_book'
+  resources :books
   root 'books#index' # ルートパスをbooksコントローラーのindexアクションに指定
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

d1178dcc6b5bf6695b6ee2f7d88440c21ff8f0fa
yoshihiro201104 <187289075+yoshihiro201104@users.noreply.github.com>
2024-12-05 10:02:34 +0000

    [Add]book

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -23,22 +23,34 @@ class BooksController < ApplicationController
 
   def show
     @book = Book.find(params[:id])
+    
   end
 
   def edit
     @book = Book.find(params[:id])
-    flash[:error_messages] = @book.errors.full_messages if @book.errors.any?
+    
+    # 成功時のメッセージ
+    flash[:notice] = "Book was successfully updated."
+    
+
+    
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)  
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "Book was successfully updated."
+      redirect_to book_path(@book)
+    else
+      flash[:alert] = "Title or Body can't be blank."
+      render :edit
+    end
   end
 
   def destroy
     book = Book.find(params[:id])  # データ（レコード）を1件取得
     book.destroy  # データ（レコード）を削除
+    flash[:notice] = "Book was successfully destroyed."
     redirect_to '/books'  # 投稿一覧画面へリダイレクト 
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,5 @@
 Rails.application.routes.draw do
-  get 'books/new'
-  post 'books' => 'books#create' #ここを追記します
-  get 'books' => 'books#index'
-  get 'books/:id' => 'books#show', as: 'book'
-  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
-  get 'books/:id' => 'books#show'
-  get 'books/edit'
   resources :books
   get 'top' => 'homes#top'
-  patch 'books/:id' => 'books#update', as: 'update_book'
-  delete 'book/:id' => 'books#destroy', as: 'destroy_book'
-  resources :books
-  root 'homes#top' # ルートパスをbooksコントローラーのindexアクションに指定
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'homes#top'
 end
